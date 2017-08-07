895338c4643410c1cdece554aacd8bb8d050586b
Ali rabie <com.appsmatic.sedra>
2017-08-07 15:12:59 +0200

    Active account screen , Reset Password , Security Code UI

---
+++ app/src/main/java/rihanna/appsmatic/com/rihanna/Activities/SecurityCodeFPscreen.java
@@ -0,0 +1,30 @@
+package rihanna.appsmatic.com.rihanna.Activities;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
+
+import rihanna.appsmatic.com.rihanna.R;
+
+public class SecurityCodeFPscreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.security_code_fp_screen);
+
+        Window window = this.getWindow();
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        //Check Os Ver For Set Status Bar
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            window.setStatusBarColor(ContextCompat.getColor(this, R.color.colorPrimary));
+        }
+
+
+
+    }
+}
