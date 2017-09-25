9c05d486d02f0abf9e1875dafbd4df3fc33a6097
Ali rabie <com.appsmatic.sedra>
2017-09-25 05:04:28 +0200

    implement facebook & twitter & gmail login integrations

--- app/src/main/java/rihanna/appsmatic/com/rihanna/Activities/SignInScreen.java
+++ app/src/main/java/rihanna/appsmatic/com/rihanna/Activities/SignInScreen.java
@@ -2,10 +2,14 @@ package rihanna.appsmatic.com.rihanna.Activities;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.view.Window;
@@ -14,12 +18,43 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.gitonway.lee.niftymodaldialogeffects.lib.Effectstype;
 import com.gitonway.lee.niftymodaldialogeffects.lib.NiftyDialogBuilder;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.twitter.sdk.android.core.DefaultLogger;
+import com.twitter.sdk.android.core.OAuthSigning;
+import com.twitter.sdk.android.core.Result;
+import com.twitter.sdk.android.core.Twitter;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import com.twitter.sdk.android.core.TwitterAuthToken;
+import com.twitter.sdk.android.core.TwitterConfig;
+import com.twitter.sdk.android.core.TwitterCore;
+import com.twitter.sdk.android.core.TwitterException;
+import com.twitter.sdk.android.core.TwitterSession;
+import com.twitter.sdk.android.core.identity.TwitterLoginButton;
+import com.twitter.sdk.android.core.models.Configuration;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -34,19 +69,47 @@ import rihanna.appsmatic.com.rihanna.API.WebServiceTools.RihannaAPI;
 import rihanna.appsmatic.com.rihanna.Prefs.SaveSharedPreference;
 import rihanna.appsmatic.com.rihanna.R;
 
-public class SignInScreen extends AppCompatActivity {
+public class SignInScreen extends AppCompatActivity  {
 
-    private EditText user,pass;
-    private TextView forgetPassBtn,createNewAccount,login;
+    private EditText user, pass;
+    private TextView forgetPassBtn, createNewAccount, login;
     private CheckBox remeberMe;
-
+    private LoginButton fbLoginButton;
+    private TwitterLoginButton twitterLoginButton;
+    private SignInButton signInButton;
+    private CallbackManager callbackManager;
+    private final String CONSUMER_KEY = "Cx1OokZ9Zf7CICMcjU6rfdUSh";
+    private final String CONSUMER_SECRET = "ZzCgAxMuQf1XoOlnIngs5VL4o4WtGve6hvyw3tPpBKEcppk6i9";
+    GoogleApiClient mGoogleApiClient;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         overridePendingTransition(R.anim.fadein, R.anim.fadeout);
         super.onCreate(savedInstanceState);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+
+        TwitterConfig config = new TwitterConfig.Builder(this)
+                .logger(new DefaultLogger(Log.DEBUG))
+                .twitterAuthConfig(new TwitterAuthConfig(CONSUMER_KEY, CONSUMER_SECRET))
+                .debug(true)
+                .build();
+        Twitter.initialize(config);
+
+
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(this);
+        callbackManager = CallbackManager.Factory.create();
         setContentView(R.layout.activity_sign_in_screen);
+
 
         Window window = this.getWindow();
         window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
@@ -56,14 +119,20 @@ public class SignInScreen extends AppCompatActivity {
             window.setStatusBarColor(ContextCompat.getColor(this, R.color.colorPrimary));
         }
 
-        //Setup items
-        forgetPassBtn=(TextView)findViewById(R.id.forgetpassbtn);
-        createNewAccount=(TextView)findViewById(R.id.create_account_btn);
-        login=(TextView)findViewById(R.id.login_btn);
-        user=(EditText)findViewById(R.id.email_input_login);
-        pass=(EditText)findViewById(R.id.password_input_login);
-        remeberMe=(CheckBox)findViewById(R.id.remeberlogincheck);
 
+        printKeyHash();
+
+
+        //Setup items
+        forgetPassBtn = (TextView) findViewById(R.id.forgetpassbtn);
+        createNewAccount = (TextView) findViewById(R.id.create_account_btn);
+        login = (TextView) findViewById(R.id.login_btn);
+        user = (EditText) findViewById(R.id.email_input_login);
+        pass = (EditText) findViewById(R.id.password_input_login);
+        remeberMe = (CheckBox) findViewById(R.id.remeberlogincheck);
+        fbLoginButton = (LoginButton) findViewById(R.id.facebook_login_btn);
+        twitterLoginButton = (TwitterLoginButton) findViewById(R.id.twitter_login_btn);
+        signInButton = (SignInButton) findViewById(R.id.gmail_login_btn);
 
 
         //Check Os Ver
@@ -71,7 +140,6 @@ public class SignInScreen extends AppCompatActivity {
             forgetPassBtn.setBackgroundResource(R.drawable.ripple);
             createNewAccount.setBackgroundResource(R.drawable.ripple);
         }
-
 
 
         //login btn action
@@ -102,13 +170,11 @@ public class SignInScreen extends AppCompatActivity {
                     loginData.put("password", pass.getText().toString() + "");
 
 
-                    if(remeberMe.isChecked()){
-                        SaveSharedPreference.setUserName(SignInScreen.this,loginData.get("email").toString()+"",loginData.get("password").toString()+"");
-                    }else {
-                        SaveSharedPreference.setUserName(SignInScreen.this,"","");
+                    if (remeberMe.isChecked()) {
+                        SaveSharedPreference.setUserName(SignInScreen.this, loginData.get("email").toString() + "", loginData.get("password").toString() + "");
+                    } else {
+                        SaveSharedPreference.setUserName(SignInScreen.this, "", "");
                     }
-
-
 
 
                     //request login from server
@@ -121,8 +187,8 @@ public class SignInScreen extends AppCompatActivity {
                                     mProgressDialog.dismiss();
 
                                 if (response.body().getCustomers() != null) {
-                                    SaveSharedPreference.setCustomerId(SignInScreen.this,response.body().getCustomers().get(0).getId());
-                                    SaveSharedPreference.setCustomerInfo(SignInScreen.this,response.body());
+                                    SaveSharedPreference.setCustomerId(SignInScreen.this, response.body().getCustomers().get(0).getId());
+                                    SaveSharedPreference.setCustomerInfo(SignInScreen.this, response.body());
                                     startActivity(new Intent(SignInScreen.this, Home.class));
                                     Toast.makeText(getApplicationContext(), getResources().getString(R.string.loginsucsess), Toast.LENGTH_LONG).show();
                                     SignInScreen.this.finish();
@@ -168,9 +234,6 @@ public class SignInScreen extends AppCompatActivity {
         });
 
 
-
-
-
         //forget password button
         forgetPassBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -197,7 +260,7 @@ public class SignInScreen extends AppCompatActivity {
                                 if (response.body().getErrorMessage().equals("") || response.body().getErrorMessage() == null) {
                                     if (mProgressDialog.isShowing())
                                         mProgressDialog.dismiss();
-                                    Toast.makeText(getApplicationContext(), getResources().getString(R.string.newpasswillsend ) + " ", Toast.LENGTH_LONG).show();
+                                    Toast.makeText(getApplicationContext(), getResources().getString(R.string.newpasswillsend) + " ", Toast.LENGTH_LONG).show();
 
                                 } else {
                                     Toast.makeText(getApplicationContext(), response.body().getErrorMessage() + "", Toast.LENGTH_LONG).show();
@@ -226,7 +289,6 @@ public class SignInScreen extends AppCompatActivity {
         });
 
 
-
         //new account button
         createNewAccount.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -239,9 +301,102 @@ public class SignInScreen extends AppCompatActivity {
         });
 
 
+        //Facebook Login Button
+        fbLoginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Toast.makeText(SignInScreen.this, "Success" + "Facebook Login  :  " + loginResult.getAccessToken().getUserId(), Toast.LENGTH_LONG).show();
+            }
 
+            @Override
+            public void onCancel() {
+                Toast.makeText(SignInScreen.this, "Login Cancel", Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onError(FacebookException e) {
+                Toast.makeText(SignInScreen.this, "Facebook " + e.getMessage(), Toast.LENGTH_LONG).show();
+                Log.e("ERRRRRR", e.getMessage());
+            }
+        });
+
+
+        //Twitter Login Button
+        twitterLoginButton.setCallback(new com.twitter.sdk.android.core.Callback<TwitterSession>() {
+            @Override
+            public void success(Result<TwitterSession> result) {
+                Toast.makeText(SignInScreen.this, "Success" + "Twitter Login  :  " + result.data.getUserId()+" "+result.data.getUserName(), Toast.LENGTH_LONG).show();
+
+            }
+
+            @Override
+            public void failure(TwitterException exception) {
+                Toast.makeText(SignInScreen.this, "Twitter " + exception.getMessage(), Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+
+        //Gmail login button
+        signInButton.setSize(SignInButton.SIZE_STANDARD);
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+                startActivityForResult(signInIntent, 9001);
+            }
+        });
 
 
 
     }
+
+
+    public void printKeyHash() {
+        // Add code to print out the key hash
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo("rihanna.appsmatic.com.rihanna", PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.e("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+            Log.e("KeyHash:", e.toString());
+        } catch (NoSuchAlgorithmException e) {
+            Log.e("KeyHash:", e.toString());
+        }
+    }
+
+
+
+
+
+
+
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        twitterLoginButton.onActivityResult(requestCode, resultCode, data);
+
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == 9001) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            if (result.isSuccess()) {
+                // Signed in successfully, show authenticated UI.
+                GoogleSignInAccount acct = result.getSignInAccount();
+
+                Toast.makeText(SignInScreen.this, "Success" + "Gmail Login  :  " + acct.getId().toString()+" "+acct.getDisplayName(), Toast.LENGTH_LONG).show();
+
+            } else {
+                Toast.makeText(SignInScreen.this, "Error" + " Gmail Login ", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+
+
 }
